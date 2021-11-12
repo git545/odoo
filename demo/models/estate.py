24b127ad6d192d9980c41849f0c0a586c38550f7
git545 <sanjay78950@gmail.com>
2021-11-12 09:01:42 +0530

    addedd

--- demo/models/estate.py
+++
@@ -1,5 +0,0 @@
-from odoo import models, fields, api
-class Estate(models.Model):
-    _name = 'demo.estate'
-   
-    
--- gvp_website/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-from . import memories_model
-from . import event_model
