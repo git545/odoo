12d2f5d03d4967935f9ec7558e57ab594bbc1b3e
git545 <sanjay78950@gmail.com>
2021-11-12 08:09:42 +0530

    gooo

--- models/library_book_info.py
+++ models/library_book_info.py
@@ -1,6 +1,25 @@
 
 from odoo import models, fields
 
+class Books(models.Model):
+	_name = 'books.lib'
+	_description = 'Books'
+	_sql_constraints = [('isbn_unique', 'unique(isbn)', 'Duplicate isbn not allowed')]
+
+
+	name = fields.Char()
+	price = fields.Float()
+	# author_ids = fields.One2many('author', 'book_id')
+	author_ids = fields.Many2many('author.lib')
+	isbn = fields.Integer()
+	category_id = fields.Many2one('book.category.lib')
+	department_id = fields.Many2one('book.department.lib')
+	barcode = fields.Char()
+	publisher_id = fields.Many2one('book.publisher.lib')
+	edition = fields.Char() # Many 2 one 
+	date = fields.Date()
+	shelf_id = fields.Many2one('shelf.lib')
+
 class Author(models.Model):
 	_name = 'author.lib'
 	_description = 'Author'
@@ -33,31 +52,13 @@ class Rack(models.Model):
 	_description = 'Rack'
 
 	name = fields.Char()
-	#sheld_ids = fields.One2many('rack.lib', 'rack_id')
+	sheld_ids = fields.One2many('rack.lib', 'rack_id')
 
 class Shelf(models.Model):
 	_name = 'shelf.lib'
 	_description = 'Shelf'
 
 	name = fields.Char()
-	#rack_id = fields.Many2one('rack.lib')
+	rack_id = fields.Many2one('rack.lib')
 
 
-class Books(models.Model):
-	_name = 'books.lib'
-	_description = 'Books'
-	_sql_constraints = [('isbn_unique', 'unique(isbn)', 'Duplicate isbn not allowed')]
-
-
-	name = fields.Char()
-	price = fields.Float()
-	# author_ids = fields.One2many('author', 'book_id')
-	author_ids = fields.Many2many('author.lib')
-	isbn = fields.Integer()
-	category_id = fields.Many2one('book.category.lib')
-	department_id = fields.Many2one('book.department.lib')
-	barcode = fields.Char()
-	publisher_id = fields.Many2one('book.publisher.lib')
-	edition = fields.Char() # Many 2 one 
-	date = fields.Date()
-	shelf_id = fields.Many2one('shelf.lib')
